e93e6bba5e4a4f121eb897ab2b84f4cf34b08a64
ssaturgo <personal@ssaturgo.com>
2025-08-12 21:26:39 -0500

    added keybinds
    added more commit and push keybinds

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,5 +3,7 @@ return {
 
 	config = function()
 		vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
+		vim.keymap.set("n", "<leader>gc", "<cmd>Git commit<cr>");
+		vim.keymap.set("n", "<leader>gP", "<cmd>Git push<cr>");
 	end
 }
