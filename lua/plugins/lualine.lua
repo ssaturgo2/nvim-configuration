5a08e181ee4cb8002cde16c1c804bae22a1d74a1
ssaturgo <personal@ssaturgo.com>
2025-08-13 21:11:36 -0500

    changed theme to auto

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
     config = function ()
     	require('lualine').setup {
 		options = {
-			theme = 'gruvbox-material',
+			theme = 'auto',
 			component_separators = { left = '|', right = '|'},
 			section_separators = { left = ' ', right = ' '},
 		},
