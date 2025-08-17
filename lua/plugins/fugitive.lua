6f64d4b54b5a566515239f6aa10a3c7aeccdddcd
ssaturgo <personal@ssaturgo.com>
2025-08-16 19:07:16 -0500

    deleted fugitive

--- lua/plugins/fugitive.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"tpope/vim-fugitive",
-
-	config = function()
-		vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
-		vim.keymap.set("n", "<leader>gc", "<cmd>Git commit<cr>");
-		vim.keymap.set("n", "<leader>gp", "<cmd>Git push<cr>");
-	end
-}
